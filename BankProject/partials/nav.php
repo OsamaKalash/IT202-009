1d0eee27095cb46aa59e76d3448a610122a81f47
Osama Kalash <ok44@njit.edu>
2020-11-18 20:45:14 -0500

    updated nav

--- BankProject/partials/nav.php
+++ BankProject/partials/nav.php
@@ -17,6 +17,11 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="test_list_transactions.php">View Transaction</a></li>
         <?php endif; ?>
         <?php if (is_logged_in()): ?>
+			<li><a href="#">Create Account</a></li>
+			<li><a href="#">Accounts</a></li>
+			<li><a href="#">Deposit</a></li>
+			<li><a href="#">Withdraw</a></li>
+			<li><a href="#">Transfer</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="logout.php">Logout</a></li>
         <?php endif; ?>
