f4b806cc83b23a31339b43f1c834ee5f351fb50c
Osama Kalash <ok44@njit.edu>
2020-11-26 17:00:39 -0500

    finished view acc

--- BankProject/view_account.php
+++ BankProject/view_account.php
@@ -18,23 +18,80 @@ if (isset($id)) {
         $e = $stmt->errorInfo();
         flash($e[2]);
     }
+	
+	$stmt = $db->prepare("SELECT id, act_src_id, act_dest_id, amount, action_type, memo,expected_total from Transactions WHERE act_src_id = :id  LIMIT 10");
+    $r = $stmt->execute([":id" => $id]);
+	if ($r) {
+        $transR = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results");
+    }
 }
+
+
+
+
+
+
 ?>
 <?php if (isset($result) && !empty($result)): ?>
     <div class="card">
+	
         <div class="card-title">
             <p><b>Account Info:</b></p>
         </div>
+		
         <div class="card-body">
+		
             <div>
 				<div>Account Number: <?php safer_echo($result["account_number"]); ?></div>
                 <div>Account Type: <?php getAccType($result["account_type"]); ?></div>
                 <div>Balance: <?php safer_echo($result["balance"]); ?></div>
                 <div>Date Opened: <?php safer_echo($result["opened_date"]); ?></div>
-                <div>Last Updated: <?php safer_echo($result["last_updated"]); ?></div>
-               
+                
             </div>
+			
         </div>
+		
+		<div class="card-title">
+            <p><b>Transaction History:</b></p>
+        </div>
+		
+		<div class="results">
+    <?php if (count($transR) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($transR as $r): ?>
+                <div class="list-group-item">
+					<div>
+                        <div>Transaction Type:</div>
+                        <div><?php safer_echo($transR["action_type"]); ?></div>
+                    </div>	
+					<div>
+                        <div>Amount:</div>
+                        <div><?php safer_echo($transR["amount"]); ?></div>
+                    </div>
+					<div>
+						<div>Memo:</div>
+						<div><?php safer_echo($transR["memo"]); ?></div>
+					</div>
+					<div>
+						<div>Date Created:</div>
+						<div><?php safer_echo($transR["created"]); ?></div>
+					</div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
+
+
+
+
+
+		
     </div>
 <?php else: ?>
     <p>Error looking up id...</p>
