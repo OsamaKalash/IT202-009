0a97e47787e825aa5446f6df226dc3a20cd90e17
Osama Kalash <ok44@njit.edu>
2020-11-25 19:44:02 -0500

    define acc num

--- BankProject/create_account.php
+++ BankProject/create_account.php
@@ -75,8 +75,6 @@ if(isset($_POST["save"])){
 	
 	
 	$world_id = $db->prepare("SELECT id FROM Accounts WHERE account_number = '000000000000' ");
-	$r = $stmt->execute([
-	]);
 	$world_id = $world_id->fetch(PDO::FETCH_NUM);
 	switch($account_type){
 		
@@ -84,6 +82,7 @@ if(isset($_POST["save"])){
 		
 			$newAccID = $db->prepare("SELECT id FROM Accounts WHERE account_number = :account_number");
 			$r = $stmt->execute([
+			":account_number" => $account_number
 			]);
 			$newAccID = $newAccID->fetch(PDO::FETCH_NUM);
 			
