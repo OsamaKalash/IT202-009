10a91f28fa512381747e34671c17a631cb5a9f87
Osama Kalash <ok44@njit.edu>
2020-12-10 16:14:31 -0500

    fix table

--- BankProject/sql/006alter_users_firstname_lastname.sql
+++ BankProject/sql/006alter_users_firstname_lastname.sql
@@ -1,3 +1,3 @@
 ALTER TABLE Users
-    ADD COLUMN first_name varchar(60) default '';
-	ADD COLUMN last_name varchar(60) default '';
+    ADD (first_name varchar(60) default '', 
+	last_name varchar(60) default '')
